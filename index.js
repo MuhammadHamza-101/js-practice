d580fa3fb34abb100f0498ff39ef8e88c2b7e7a0
Muhammad-Hamza <77409746+MuhammadHamza-101@users.noreply.github.com>
2022-06-18 20:31:50 +0500

    commit

---
+++ index.js
@@ -0,0 +1,121 @@
+// const arr = [
+//   "name",
+//   true,
+//   123,
+//   { country: "Finland", city: "Helsinki" },
+//   { skills: ["HTML", "CSS", "JS", "React", "Python"] },
+// ];
+// console.log(arr);
+
+// let js = "javascript";
+// let charac = js.split("");
+// let joint = charac.join();
+// console.log("hamza", joint);
+
+// let companiesString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+// let companies = companiesString.split(",");
+// let com = companies.join();
+// console.log(com);
+
+// const fruits = ["banana", "orange", "mango", "lemon"];
+
+// let first = fruits[0];
+// console.log(first);
+// const second = fruits[1];
+// console.log(second);
+// const third = fruits[2];
+// console.log(third);
+// let lastindex = fruits.length - 1;
+// let lastfruit = fruits[lastindex]
+
+// const numbers = [0, 3.14, 9.81, 37, 98.6, 100];
+
+// console.log(numbers.length);
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+
+// lastnumber = numbers.length - 1;
+// console.log(numbers[lastnumber]);
+
+// const webTechs = [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Redux',
+//     'Node',
+//     'MongoDB',
+//   ] 
+  
+//   console.log(webTechs) 
+//   console.log(webTechs.length) 
+//   console.log(webTechs[0]) 
+//   console.log(webTechs[6]) 
+  
+//   let lastIndex = webTechs.length - 1
+//   console.log(webTechs[lastIndex]) 
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya',
+//   ] // List of countries
+  
+//   console.log(countries) // -> all countries in array
+//   console.log(countries[0]) //  -> Albania
+//   console.log(countries[10]) //  -> Kenya
+  
+//   let lastIndex = countries.length - 1
+//   console.log(countries[lastIndex]) //  -> Kenya
+
+// const shoppingCart = [
+//     'Milk',
+//     'Mango',
+//     'Tomato',
+//     'Potato',
+//     'Avocado',
+//     'Meat',
+//     'Eggs',
+//     'Sugar',
+//   ] // List of food products
+  
+//   console.log(shoppingCart) // -> all shoppingCart in array
+//   console.log(shoppingCart[0]) //  -> Milk
+//   console.log(shoppingCart[7]) //  -> Sugar
+  
+//   let lastIndex = shoppingCart.length - 1
+//   console.log(shoppingCart[lastIndex]) //  -> Sugar
+
+// const numbers = [1, 2, 3, 4, 5]
+// numbers[0] = 10 // changing 1 at index 0 to 10
+// numbers[1] = 20 // changing  2 at index 1 to 20
+
+// console.log(numbers) // [10, 20, 3, 4, 5]
+
+// const countries = [
+//   'Albania',
+//   'Bolivia',
+//   'Canada',
+//   'Denmark',
+//   'Ethiopia',
+//   'Finland',
+//   'Germany',
+//   'Hungary',
+//   'Ireland',
+//   'Japan',
+//   'Kenya',
+// ]
+
+// countries[0] = 'Afghanistan' // Replacing Albania by Afghanistan
+// let lastIndex = countries.length - 1
+// countries[lastIndex] = 'Korea' // Replacing Kenya by Korea
+
+// console.log(countries)
